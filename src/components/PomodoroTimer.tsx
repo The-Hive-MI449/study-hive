fb2268da4b2569f62cbcaf07a5b02c465008a01e
rhyanmarshall <marsh608@msu.edu>
2026-04-14 19:20:12 +0000

    timer fixed

--- src/components/PomodoroTimer.tsx
+++ src/components/PomodoroTimer.tsx
@@ -9,7 +9,7 @@ export function PomodoroTimer() {
   const [isBreak, setIsBreak] = useState(false)
 
   useEffect(() => {
-    let interval: NodeJS.Timeout
+    let interval: ReturnType<typeof setInterval>
 
     if (isRunning && timeLeft > 0) {
       interval = setInterval(() => {
