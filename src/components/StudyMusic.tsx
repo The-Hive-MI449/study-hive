6bdbb854677623558edb96bfc72a8387fb0dc5c5
perzcars <perzcars@msu.edu>
2026-04-26 19:19:36 +0000

    spotify player reloads every time

--- src/components/StudyMusic.tsx
+++ src/components/StudyMusic.tsx
@@ -2,30 +2,39 @@ import { useEffect } from 'react';
 
 export function StudyMusic() {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://open.spotify.com/embed/iframe-api/v1';
-    script.async = true;
-    document.body.appendChild(script);
-
-    window.onSpotifyIframeApiReady = (IFrameAPI: any) => {
+    const loadSpotifyPlayer = () => {
       const element = document.getElementById('embed-iframe');
       const options = {
-        uri: 'https://open.spotify.com/playlist/6zCID88oNjNv9zx6puDHKj?si=eJJjK8AcT1e15CU3jwF_Eg',
+        uri: 'https://open.spotify.com/playlist/6zCID88oNjNv9zx6puDHKj?si=OrEb2KdmTFWZeSPFg7VW5A',
         width: '100%',
-        height: 480
+        height: 380
       };
       const callback = (EmbedController: any) => {
         console.log('Spotify player loaded!', EmbedController);
       };
       
-      if (element) {
-        IFrameAPI.createController(element, options, callback);
+      if (element && (window as any).IFrameAPI) {
+        (window as any).IFrameAPI.createController(element, options, callback);
       }
     };
 
-    return () => {
-      document.body.removeChild(script);
-    };
+    if ((window as any).IFrameAPI) {
+      loadSpotifyPlayer();
+    } else {
+      const existingScript = document.querySelector('script[src="https://open.spotify.com/embed/iframe-api/v1"]');
+      
+      if (!existingScript) {
+        const script = document.createElement('script');
+        script.src = 'https://open.spotify.com/embed/iframe-api/v1';
+        script.async = true;
+        document.body.appendChild(script);
+      }
+
+      window.onSpotifyIframeApiReady = (IFrameAPI: any) => {
+        (window as any).IFrameAPI = IFrameAPI;
+        loadSpotifyPlayer();
+      };
+    }
   }, []);
 
   return (
